79c7f842544fb6d4263bfef42711f1a36e32f9d3
petrelevich <petrelevich@yandex.ru>
2024-11-19 13:10:47 +0300

    L05-collections

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,8 @@ include("L02-gradle2-libApiUse")
 include("L02-logging")
 
 include("L03-generics")
-
 include("L04-qa")
+include("L05-collections")
 
 pluginManagement {
     val jgitver: String by settings
