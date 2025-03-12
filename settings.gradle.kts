36a50f1d8ebf714d0f84f95f3ce56b7b4c0ca769
stvort <stvort@mail.ru>
2025-03-12 20:34:33 +0400

    L32-concurrentCollections added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -61,6 +61,8 @@ include ("L29-threads")
 include ("L30-JMM")
 include ("L31-executors")
 
+include ("L32-concurrentCollections")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
